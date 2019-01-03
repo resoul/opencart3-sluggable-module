ce9a0378f58690e2ec4fab2d33014190ce8724b6
resoul <resoul@bk.ru>
2019-01-03 11:56:30 +0200

    admin controls

---
+++ admin/controller/extension/module/sluggable.php
@@ -0,0 +1,63 @@
+<?php
+class ControllerExtensionModuleSluggable extends Controller
+{
+    private $error = [];
+
+    public function index()
+    {
+        $this->load->language('extension/module/sluggable');
+        $this->document->setTitle($this->language->get('heading_title'));
+        $this->load->model('setting/setting');
+
+        $data['breadcrumbs'] = [
+            [
+                'text' => $this->language->get('text_home'),
+                'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+            ],
+            [
+                'text' => $this->language->get('text_extension'),
+                'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
+            ],
+            [
+                'text' => $this->language->get('heading_title'),
+                'href' => $this->url->link('extension/module/sluggable', 'user_token=' . $this->session->data['user_token'], true)
+            ]
+        ];
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            $this->model_setting_setting->editSetting('sluggable', $this->request->post);
+
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
+        }
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        if ($this->model_setting_setting->getSettingValue('sluggable_status')) {
+            $data['sluggable_status'] = $this->model_setting_setting->getSettingValue('sluggable_status');
+        }
+
+        $data['action'] = $this->url->link('extension/module/sluggable', 'user_token=' . $this->session->data['user_token'], true);
+        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
+
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+
+        $this->response->setOutput($this->load->view('extension/module/sluggable', $data));
+    }
+
+    private function validate()
+    {
+        if (!$this->user->hasPermission('modify', 'extension/module/sluggable')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        return !$this->error;
+    }
+}
---
+++ admin/language/en-gb/extension/module/sluggable.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Sluggable';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified sluggable module!';
+$_['text_edit']        = 'Edit Sluggable Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify sluggable module!';
